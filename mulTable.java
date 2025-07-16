caeec6e952934f8db876969353b816e03eba453a
Nimishamba MD Gowda <153758807+Nimisha1610@users.noreply.github.com>
2025-07-16 11:29:43 +0530

    Create mulTable.java

---
+++ mulTable.java
@@ -0,0 +1,14 @@
+package in28MinutesRestart;
+
+public class mulTableRunner {
+	void printMul() {
+		 for(int i=1;i<=10;i++) {
+			 System.out.printf("%d*%d=%d",5,i,5*i).println();
+		 }
+	 }
+	public static void main(String[] args) {
+        mulTableRunner m=new mulTableRunner();
+        m.printMul();
+	}
+
+}

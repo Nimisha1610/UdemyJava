9c8ccd365c22d1d7e9b8ab9f5e46e2d1caa285be
Nimishamba MD Gowda <153758807+Nimisha1610@users.noreply.github.com>
2025-09-23 22:28:46 +0530

    Create Ex4LAstDigit.java

---
+++ Ex4LAstDigit.java
@@ -0,0 +1,18 @@
+public class NumberUtils {
+
+    public int getLastDigit(int number) {
+if(number<0){
+    return -1;
+}
+
+    int digit=0;
+    digit=number%10;
+return digit;
+
+
+
+
+
+
+    }
+}
